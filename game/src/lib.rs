216ab8299db976381d277bf8b795e94c27201537
GONI Guillaume <g.goni@laposte.net>
2024-10-13 10:41:11 +0200

    base player movements

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -1,12 +1,24 @@
 //! Game project.
+// use crate::bot::Bot;
 use fyrox::{
-    core::pool::Handle,
-    core::reflect::prelude::*,
-    core::visitor::prelude::*,
-    event::Event,
+    core::{
+        algebra::{Vector2, Vector3},
+        pool::Handle,
+        reflect::prelude::*,
+        type_traits::prelude::*,
+        visitor::prelude::*,
+    },
+    event::{ElementState, Event, WindowEvent},
     gui::message::UiMessage,
+    keyboard::{KeyCode, PhysicalKey},
     plugin::{Plugin, PluginContext, PluginRegistrationContext},
-    scene::Scene,
+    scene::{
+        animation::spritesheet::SpriteSheetAnimation,
+        dim2::{rectangle::Rectangle, rigidbody::RigidBody},
+        node::Node,
+        Scene,
+    },
+    script::{ScriptContext, ScriptTrait},
 };
 use std::path::Path;
 
@@ -19,8 +31,10 @@ pub struct Game {
 }
 
 impl Plugin for Game {
-    fn register(&self, _context: PluginRegistrationContext) {
+    fn register(&self, context: PluginRegistrationContext) {
         // Register your scripts here.
+        let script_constructors = &context.serialization_context.script_constructors;
+        script_constructors.add::<Player>("Player");
     }
 
     fn init(&mut self, scene_path: Option<&str>, context: PluginContext) {
@@ -59,5 +73,124 @@ impl Plugin for Game {
         _context: &mut PluginContext,
     ) {
         self.scene = scene;
+    }
+}
+
+#[derive(Visit, Reflect, Debug, Clone, TypeUuidProvider, ComponentProvider)]
+#[type_uuid(id = "c5671d19-9f1a-4286-8486-add4ebaadaec")]
+#[visit(optional)]
+struct Player {
+    sprite: Handle<Node>,
+    move_left: bool,
+    move_right: bool,
+    jump: bool,
+    animations: Vec<SpriteSheetAnimation>,
+    current_animation: u32,
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        Self {
+            sprite: Default::default(),
+            move_left: false,
+            move_right: false,
+            jump: false,
+            animations: Default::default(),
+            current_animation: 0,
+        }
+    }
+}
+
+impl ScriptTrait for Player {
+    // Called once at initialization.
+    fn on_init(&mut self, _context: &mut ScriptContext) {}
+
+    // Put start logic - it is called when every other script is already initialized.
+    fn on_start(&mut self, _context: &mut ScriptContext) {}
+
+    // Called whenever there is an event from OS (mouse click, keypress, etc.)
+    fn on_os_event(&mut self, event: &Event<()>, _context: &mut ScriptContext) {
+        if let Event::WindowEvent { event, .. } = event {
+            if let WindowEvent::KeyboardInput { event, .. } = event {
+                if let PhysicalKey::Code(keycode) = event.physical_key {
+                    let is_pressed = event.state == ElementState::Pressed;
+
+                    match keycode {
+                        KeyCode::KeyA => self.move_left = is_pressed,
+                        KeyCode::KeyD => self.move_right = is_pressed,
+                        KeyCode::Space => self.jump = is_pressed,
+                        _ => (),
+                    }
+                }
+            }
+        }
+    }
+
+    // Called every frame at fixed rate of 60 FPS.
+    fn on_update(&mut self, context: &mut ScriptContext) {
+        // The script can be assigned to any scene node, but we assert that it will work only with
+        // 2d rigid body nodes.
+        if let Some(rigid_body) = context.scene.graph[context.handle].cast_mut::<RigidBody>() {
+            let x_speed = if self.move_left {
+                3.0
+            } else if self.move_right {
+                -3.0
+            } else {
+                0.0
+            };
+
+            if x_speed != 0.0 {
+                self.current_animation = 0;
+            } else {
+                self.current_animation = 1;
+            }
+
+            if self.jump {
+                rigid_body.set_lin_vel(Vector2::new(x_speed, 4.0))
+            } else {
+                rigid_body.set_lin_vel(Vector2::new(x_speed, rigid_body.lin_vel().y))
+            };
+
+            // It is always a good practice to check whether the handles are valid, at this point we don't know
+            // for sure what's the value of the `sprite` field. It can be unassigned and the following code won't
+            // execute. A simple `context.scene.graph[self.sprite]` would just panicked in this case.
+            if let Some(sprite) = context.scene.graph.try_get_mut(self.sprite) {
+                // We want to change player orientation only if he's moving.
+                if x_speed != 0.0 {
+                    let local_transform = sprite.local_transform_mut();
+
+                    let current_scale = **local_transform.scale();
+
+                    local_transform.set_scale(Vector3::new(
+                        // Just change X scaling to mirror player's sprite.
+                        current_scale.x.copysign(-x_speed),
+                        current_scale.y,
+                        current_scale.z,
+                    ));
+                }
+            }
+        }
+        if let Some(current_animation) = self.animations.get_mut(self.current_animation as usize) {
+            current_animation.update(context.dt);
+
+            if let Some(sprite) = context
+                .scene
+                .graph
+                .try_get_mut(self.sprite)
+                .and_then(|n| n.cast_mut::<Rectangle>())
+            {
+                // Set new frame to the sprite.
+                sprite
+                    .material()
+                    .data_ref()
+                    .set_texture(&"diffuseTexture".into(), current_animation.texture())
+                    .unwrap();
+                sprite.set_uv_rect(
+                    current_animation
+                        .current_frame_uv_rect()
+                        .unwrap_or_default(),
+                );
+            }
+        }
     }
 }
